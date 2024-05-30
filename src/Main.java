78dd49458cdccdba4b47be4552e134bd2a864a23
Ithan <ithan.camacho@epn.edu.ec>
2024-05-30 08:45:01 -0500

    Nuevas instancias para Procesadores moviles

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,18 @@
 import Procesadores.Procesadores;
+import Procesadores.Procesadores_Moviles;
 
 public class Main {
     public static void main(String[] args) {
 
-        Procesadores p1 = new Procesadores();
+    //Interante1
+
+        Procesadores_Moviles proc1 = new Procesadores_Moviles();
+
+        Procesadores_Moviles proc2 = new Procesadores_Moviles(8,"Snapdragon 8 Gen 2",3.2,3.0,true);
+
+    //Integrante2
+
+
+
     }
 }
