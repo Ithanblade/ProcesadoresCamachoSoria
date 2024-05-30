5e5544f02bb4e761b22937dfe66841d30d7559f9
Ithan <ithan.camacho@epn.edu.ec>
2024-05-30 08:01:25 -0500

    a1

---
+++ src/Procesadores/Procesadores_Moviles.java
@@ -0,0 +1,26 @@
+package Procesadores;
+
+public class Procesadores_Moviles extends Procesadores{
+
+    Boolean optimizacion_energia;
+
+    public Procesadores_Moviles() {
+    }
+
+    public Procesadores_Moviles(int nucleos, String nombre, Double velocidad, Double consumo, Boolean optimizacion_energia) {
+        super(nucleos, nombre, velocidad, consumo);
+        this.optimizacion_energia = optimizacion_energia;
+    }
+
+    public Boolean getOptimizacion_energia() {
+        return optimizacion_energia;
+    }
+
+    public void setOptimizacion_energia(Boolean optimizacion_energia) {
+        this.optimizacion_energia = optimizacion_energia;
+    }
+
+    void imprimir_datos_movil(){
+        System.out.println(this.optimizacion_energia);
+    }
+}
